fd9db0151f061111b39d56b200b8bbed6ff6031a
technothecow <krashmisha@gmail.com>
2021-02-02 21:00:46 +0300

    mapsAPI 0.2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from UI import Ui_MainWindow
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 import sys
 from request import generate_map
@@ -18,6 +18,14 @@ class Window(Ui_MainWindow, QMainWindow):
             self.label.setPixmap(self.pixmap)
         else:
             self.statusbar.showMessage('Request error', 5000)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        if event.key() == 16777238 and float(self.lineEdit_3.text()) < 90:
+            self.lineEdit_3.setText(str(float(self.lineEdit_3.text()) * 2))
+            self.button_clicked()
+        elif event.key() == 16777239 and float(self.lineEdit_3.text()) > 0.01:
+            self.lineEdit_3.setText(str(float(self.lineEdit_3.text()) / 2))
+            self.button_clicked()
 
 
 app = QApplication(sys.argv)
